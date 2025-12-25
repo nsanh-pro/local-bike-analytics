7246142a593aee6b5fbf0516ba3855ff0a05c517
Nicolas Sanh <nicolas.sanh.pro@gmail.com>
2025-12-25 17:50:39 +0000

    feat: add mrt_sales_staff_performance + new columns in dim_staff

--- models/core/dim_staff.sql
+++ models/core/dim_staff.sql
@@ -6,8 +6,14 @@ WITH
     ),
     final AS 
     (
-        SELECT *
-        FROM stg_staffs
+        SELECT
+            staff.*,
+            staff.first_name || ' ' || staff.last_name AS full_name,
+            store.store_name,
+            manager.first_name || ' ' || staff.last_name AS manager_full_name
+        FROM stg_staffs staff 
+        LEFT JOIN {{ ref('stg_local_bike_operational_database__stores')}} store ON staff.store_id = store.store_id
+        LEFT JOIN stg_staffs manager ON staff.manager_id = manager.staff_id
     )
 SELECT *
 FROM final
---
+++ models/marts/mrt_sales_staff_performance.sql
@@ -0,0 +1,51 @@
+WITH
+    staff AS
+    (
+        SELECT
+            staff_id,
+            full_name AS staff_full_name,
+            store_name,
+            manager_full_name
+        FROM {{ ref('dim_staff') }}
+    ),
+    monthly_orders_per_staff AS 
+    (
+        SELECT 
+            staff_id,
+            order_date.year,
+            order_date.month,
+            order_date.month_name,
+            COUNT(1) AS total_orders,
+            SUM(CASE WHEN orders.is_shipped_late THEN 1 END) AS total_late_shippings,
+            SUM(orders.total_quantity) AS total_quantity,
+            ROUND(SUM(orders.total_gross_amount), 2) AS total_gross_amount,
+            ROUND(SUM(orders.total_discount_amount), 2) AS total_discount_amount,
+            ROUND(SUM(orders.total_net_amount), 2) AS total_net_amount,
+        FROM {{ ref('fct_order') }} AS orders
+        LEFT JOIN {{ ref('dim_date') }} AS order_date ON orders.order_date_key = order_date.date_key
+        GROUP BY 
+            staff_id,
+            order_date.year,
+            order_date.month,
+            order_date.month_name
+    ),
+    final AS
+    (
+        SELECT
+            staff.staff_full_name,
+            staff.store_name,
+            staff.manager_full_name,
+            orders.year,
+            orders.month,
+            orders.month_name,
+            orders.total_orders,
+            orders.total_late_shippings,
+            orders.total_quantity,
+            orders.total_gross_amount,
+            orders.total_discount_amount,
+            orders.total_net_amount
+        FROM staff
+        LEFT JOIN monthly_orders_per_staff orders ON staff.staff_id = orders.staff_id
+    )
+SELECT *
+FROM final
